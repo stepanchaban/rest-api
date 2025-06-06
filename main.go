d86cf665fc46678a96385fa721dd8dc38c066c69
Stepan Chaban <xxlnamne@gmail.com>
2025-06-06 14:00:22 +0300

    add orm postgres

--- main.go
+++ main.go
@@ -1,22 +1,52 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+
+var db *gorm.DB
+
+func initDB() {
+	dsn :="host=localhost user=postgres password=yourpassword dbname=postgres port=5432 sslmode=disable"
+	var err error
+
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Could not connect to database: %v", err)
+	}
+
+	if err := db.AutoMigrate(&Task{}); err != nil {
+		log.Fatalf("Could not migrate: %v", err)
+	}
+}
+
 type Task struct {
-	ID   string `json:"id"`
+	ID   string `gorm:"primaryKey" json:"id"`
 	Task string `json:"task"`
+	IsDone bool `json:"is_done"`
 }
 
 type RequestBody struct {
 	Task string `json:"task"`
+	IsDone bool `json:"is_done"`
 }
 
-var tasks []Task
+func getHandler(c echo.Context) error {
+	var task []Task
+
+	if err := db.Find(&task).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Could not get tasks"})
+	}
+
+	return c.JSON(http.StatusOK, task)
+}
 
 func postHandler(c echo.Context) error {
 	var req RequestBody
@@ -29,16 +59,17 @@ func postHandler(c echo.Context) error {
 	newTask := Task{
 		ID:   uuid.NewString(),
 		Task: req.Task,
+		IsDone: req.IsDone,
 	}
 
-	tasks = append(tasks, newTask)
+	if err := db.Create(&newTask).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Could not create task"})
+	}
 
 	return c.JSON(http.StatusCreated, newTask)
 }
 
-func getHandler(c echo.Context) error {
-	return c.JSON(http.StatusOK, tasks)
-}
+
 
 func patchHandler(c echo.Context) error {
 	id := c.Param("id")
@@ -50,42 +81,40 @@ func patchHandler(c echo.Context) error {
 		})
 	}
 
-	for i, t := range tasks {
-		if t.ID == id {
-			if req.Task != "" {
-				tasks[i].Task = req.Task
-			}
-			return c.JSON(http.StatusOK, tasks[i])
-		}
+	var task Task
+
+	if err := db.First(&task, "id = ?", id).Error; err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Could not find task"})
 	}
 
-	return c.JSON(http.StatusNotFound, map[string]string{
-		"error": "Task not found",
-	})
+	task.Task = req.Task
+	task.IsDone = req.IsDone
+
+	if err := db.Save(&task).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Could not update task"})
+	}
+
+	return c.JSON(http.StatusOK, task)
 }
 
 func deleteHandler(c echo.Context) error {
 	id := c.Param("id")
 
-	for i, t := range tasks {
-		if t.ID == id {
-			tasks = append(tasks[:i], tasks[i+1:]...)
-			return c.NoContent(http.StatusNoContent)
-		}
+	if err := db.Delete(&Task{}, "id = ?", id).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Could not delete task"})
 	}
 
-	return c.JSON(http.StatusNotFound, map[string]string{
-		"error": "Task not found",
-	})
+	return c.NoContent(http.StatusNoContent)
 }
 
 func main() {
+	initDB()
 	e := echo.New()
 
-	e.POST("/task", postHandler)
-	e.GET("/task", getHandler)
-	e.PATCH("/task/:id", patchHandler)
-	e.DELETE("/task/:id", deleteHandler)
+	e.POST("/tasks", postHandler)
+	e.GET("/tasks", getHandler)
+	e.PATCH("/tasks/:id", patchHandler)
+	e.DELETE("/tasks/:id", deleteHandler)
 
 	e.Start("localhost:8080")
 }
