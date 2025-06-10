3858ab42cba9c4a2dfc0cf4e7f882e13bde0465b
Stepan Chaban <xxlnamne@gmail.com>
2025-06-10 11:47:59 +0300

    migration for db

---
+++ migrations/20250610083855_tasks.up.sql
@@ -0,0 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE tasks (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    task VARCHAR(255) NOT NULL,
+    is_done BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    deleted_at TIMESTAMP DEFAULT NULL
+);
